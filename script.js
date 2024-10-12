051cc2e125db1d60e47905fa9a7be7b1f01982d0
albert-murphy <albertmurphy128@gmail.com>
2024-10-12 21:43:06 +0100

    Added new page habit.html which displays in depth info on specific user habit. Made habits displayed on index.html each their own seperate link to a queried habit.html page to view specific info. Wrote a function to ensure each habit name is unique. Other small changes.

--- script.js
+++ script.js
@@ -74,7 +74,12 @@ function showHabits() {
     habits.forEach((habit) => {
 
         const li = document.createElement('li'); // Create a list item
-        li.textContent = `${habit.name} - ${habit.completed ? 'Completed' : 'Not Completed'}`; // Set the text to the habit
+        const a = document.createElement('a');  // Create a link item
+
+        a.href= `habit.html?name=${encodeURIComponent(habit.name)}` // Set the links aref to habit.html + a query with the name of habit
+        a.textContent = `${habit.name} - ${habit.completed ? 'Completed' : 'Not Completed'}`; // Set the text to the habit link
+
+        li.appendChild(a);  // Append link to the list
         ul.appendChild(li); // Append the list item to the unordered list
     });
 
@@ -87,11 +92,47 @@ function submitHabit() {    // Refers to user entering habit into text box...
 
     let habit = habitInputElement.value;
 
-    addHabit(habit);    // Push habit to local storage
-    showHabits();   // Reload habits on page
+    if(!checkHabitExists(habit)) { // Check if habit exists first to avoid duplicates...
+
+        console.log("FFW")
+
+        addHabit(habit);    // Push habit to local storage
+        showHabits();   // Reload habits on page
+    }
 
     habitInputElement.value = "";   // Set text box blank
 }
 
+function checkHabitExists(name) {
+
+    let habits = getHabits();
+    return habits.find(h => h.name === name) != null;
+}
+
+function displayHabitInformation() {
+
+    let habits = getHabits();
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const habitName = urlParams.get('name');    // retreive habit name based on url query
+
+    // Warning: this means habit names must be unique!
+    const habit = habits.find(h => h.name === habitName);   // retreive habit object based on habit name
+
+    let headerElement = document.getElementById("habit-name");
+    let statusElement = document.getElementById("habit-status");
+
+    console.log(!checkHabitExists(habitName))
+
+    if(habit) {
+        console.log(habit);      
+        headerElement.textContent = `Habit: ${habit.name}`; // Set header to equal selected habit
+        statusElement.textContent = `Status: ${habit.completed ? 'Completed' : 'Not Completed'}`;
+    }
+    else {
+        headerElement.textContent = 'Habit: Null'
+    }
+}
+
 console.log(getHabits());   // Debugging
-showHabits();   // Load habits on start...
